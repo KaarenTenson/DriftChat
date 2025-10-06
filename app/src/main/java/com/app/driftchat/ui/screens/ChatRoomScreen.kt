fa7dbb226fd907c207a2bf930142b9536995f15f
andersaa <ander.saarniit@ut.ee>
2025-10-06 12:08:23 +0300

    Component and view model for sending messages.

--- app/src/main/java/com/app/driftchat/ui/screens/ChatRoomScreen.kt
+++ app/src/main/java/com/app/driftchat/ui/screens/ChatRoomScreen.kt
@@ -1,6 +1,5 @@
 package com.app.driftchat.ui.screens
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,12 +7,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
